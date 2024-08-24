4032c633fef8587dcfc09663b3f3f56d0164f5ac
Santiago Machado <santimachado613@gmail.com>
2024-08-24 16:07:33 -0500

    Mejoras en la presentacion del juego

--- cuatroenlinea.py
+++ cuatroenlinea.py
@@ -9,6 +9,39 @@ GAME_BOARD2 = (6, 6)  # Tamaño del tablero 6x6
 # MÓDULOS
 
 # MÓDULO 1: ELEGIR TABLERO
+def intro():
+    print(
+    """
+Bienvenidos a 4 en linea.
+
+Las reglas son sencillas :
+
+-El jugador que complete cuatro fichas en línea (ya sea horizontal, vertical o diagonal) gana la partida.
+-Se triplicarán los puntos al jugador que gane dos partidas consecutivas.
+-Antes de comenzar, los jugadores deben elegir el tamaño del tablero: 4x4 o 6x6.
+-Si ninguno de los dos jugadores tiene oportunidad de ganar, aunque el tablero no este lleno, la partida terminará en empate.
+-Después de cada partida, se mostrará el puntaje acumulado de cada jugador.
+-Después de cada partida, el sistema preguntará si los jugadores desean seguir jugando.
+
+¿Estas listo para jugar?
+a.Si
+b.No""")
+
+    while True:
+        answer = input().strip().lower()
+        if answer in ["a", "b"]:
+            break
+        else:
+            print("Opción no válida. Por favor, selecciona una opción válida:")
+            print("¿Estás listo para jugar?")
+            print("a. Sí")
+            print("b. No")
+    if answer == "a":
+        return True
+    else:
+        print("Vuelve cuando estes preparado para jugar...")
+        return False
+    
 def playerName ():
     print("Jugador 1, ingresa tu nombre")
     player1 = input()
@@ -266,138 +299,136 @@ def checkWin(board):
 
 # Se usa el MÓDULO 1 para dar inicio al juego, empezando por escoger el tablero
 
-play = True
-roundWinX = []
-roundWinO = []
-pointsX = 0
-pointsO = 0
-whoWin = None
-print(
-"""
-Bienvenidos a 4 en linea.
-Jugadores, eligan sus nombres.
-""")
-player1, player2 = playerName()
-while play:
-    print("Elige una opción")
-    gameBoard = chooseGameBoard()
-    if gameBoard[0] == False:
-        print("Vuelve pronto...")
-        break
-    if gameBoard[1] == True:
-        board = gameBoard[2]  # Selecciona el tablero actual
-        printBoard(board)
-        print("Los ceros representan los espacios disponibles")
-        
-        winner = False
-        round = 0
 
-        while not winner:
-            # INTERACCIÓN JUGADOR 1
-            print(f"{player1}, elige una casilla")
-            while True:
-                rowX = checkString(input("Elige una fila:  "))
-                columnX = checkString(input("Elige una columna:  "))
+if intro():
+    play = True
+    roundWinX = []
+    roundWinO = []
+    pointsX = 0
+    pointsO = 0
+    whoWin = None
+    player1, player2 = playerName()
+    while play:
+        print("Elige una opción")
+        gameBoard = chooseGameBoard()
+        if gameBoard[0] == False:
+            print("Vuelve pronto...")
+            break
+        if gameBoard[1] == True:
+            board = gameBoard[2]  # Selecciona el tablero actual
+            printBoard(board)
+            print("Los ceros representan los espacios disponibles")
+            
+            winner = False
+            round = 0
 
-                # Verifica que las entradas de fila y columna sean válidas
-                while not rowX or not columnX:
-                    print("Entrada inválida. Elige una casilla válida")
+            while not winner:
+                # INTERACCIÓN JUGADOR 1
+                print(f"{player1}, elige una casilla")
+                while True:
                     rowX = checkString(input("Elige una fila:  "))
                     columnX = checkString(input("Elige una columna:  "))
 
-                check = checkNumber(rowX[0], columnX[0], board)
-                if check[0]:
-                    rowX, columnX = check[1:]
-                    if board[rowX-1, columnX-1] == 0:
-                        board[rowX-1, columnX-1] = 'X'
-                        break  # Salir del bucle una vez que se haya marcado una casilla válida
+                    # Verifica que las entradas de fila y columna sean válidas
+                    while not rowX or not columnX:
+                        print("Entrada inválida. Elige una casilla válida")
+                        rowX = checkString(input("Elige una fila:  "))
+                        columnX = checkString(input("Elige una columna:  "))
+
+                    check = checkNumber(rowX[0], columnX[0], board)
+                    if check[0]:
+                        rowX, columnX = check[1:]
+                        if board[rowX-1, columnX-1] == 0:
+                            board[rowX-1, columnX-1] = 'X'
+                            break  # Salir del bucle una vez que se haya marcado una casilla válida
+                        else:
+                            print("Esta casilla está ocupada, elige otra:")
                     else:
-                        print("Esta casilla está ocupada, elige otra:")
-                else:
-                    print("Casilla no disponible. Elige otra casilla")
+                        print("Casilla no disponible. Elige otra casilla")
 
-            printBoard(board)
-            if round >= 3 and checkWin(board):
-                whoWin = player1
-                pointsX += 1
-                break       
-            if round >= 3: 
-                if not isThereAWinner(board): #Se comprueba si aun hay posibilidades de ganar despues de cada vuelta.
-                    print("¡Es un empate!")
-                    break 
-            # INTERACCIÓN JUGADOR 2
-            print(f"{player2}, elige una casilla")
-            while True:
-                rowO = checkString(input("Elige una fila:  "))
-                columnO = checkString(input("Elige una columna:  "))
-
-                # Verifica que las entradas de row y columna sean válidas
-                while not rowO or not columnO:
-                    print("Entrada inválida. Elige una casilla válida")
+                printBoard(board)
+                if round >= 3 and checkWin(board):
+                    whoWin = player1
+                    pointsX += 1
+                    break       
+                if round >= 3: 
+                    if not isThereAWinner(board): #Se comprueba si aun hay posibilidades de ganar despues de cada vuelta.
+                        print("¡Es un empate!")
+                        break 
+                # INTERACCIÓN JUGADOR 2
+                print(f"{player2}, elige una casilla")
+                while True:
                     rowO = checkString(input("Elige una fila:  "))
                     columnO = checkString(input("Elige una columna:  "))
 
-                check = checkNumber(rowO[0], columnO[0], board)
-                if check[0]:
-                    rowO, columnO = check[1:]
-                    if board[rowO-1, columnO-1] == 0:
-                        board[rowO-1, columnO-1] = 'O'
-                        break  # Salir del bucle una vez que se haya marcado una casilla válida
+                    # Verifica que las entradas de row y columna sean válidas
+                    while not rowO or not columnO:
+                        print("Entrada inválida. Elige una casilla válida")
+                        rowO = checkString(input("Elige una fila:  "))
+                        columnO = checkString(input("Elige una columna:  "))
+
+                    check = checkNumber(rowO[0], columnO[0], board)
+                    if check[0]:
+                        rowO, columnO = check[1:]
+                        if board[rowO-1, columnO-1] == 0:
+                            board[rowO-1, columnO-1] = 'O'
+                            break  # Salir del bucle una vez que se haya marcado una casilla válida
+                        else:
+                            print("Esta casilla está ocupada, elige otra:")
                     else:
-                        print("Esta casilla está ocupada, elige otra:")
-                else:
-                    print("Casilla no disponible. Elige otra casilla")
+                        print("Casilla no disponible. Elige otra casilla")
 
-            printBoard(board)
+                printBoard(board)
 
-            if round >= 3 and checkWin(board):
-                whoWin = player2
-                pointsO += 1
-                break               
-            if round >= 3: 
-                if not isThereAWinner(board): #Se comprueba si aun hay posibilidades de ganar despues de cada vuelta.
-                    print("¡Es un empate!")
-                    break 
-            round+=1
-        if whoWin == player1:
-            roundWinX.append(whoWin)
-            roundWinO = []
-            whoWin = ""
-        elif whoWin == player2:
-            roundWinO.append(whoWin)
-            roundWinX = []
-            whoWin = ""
-        else:
-            roundWinO = []
-            roundWinX = []
-            whoWin = ""
-        print(f"{whoWin} gano")
-        
-        if roundWinX and len(roundWinX) == 2:
-            roundWinX = []
-            whoWin = ""
-            pointsX *= 3
-
-        elif roundWinO and len(roundWinO) == 2:
-            roundWinO = []
-            pointsO *= 3
-            whoWin = ""
-
-        print("")
-        print(f"{player1} -> {pointsX}")
-        print(f"{player2} -> {pointsO}")
-
-        # Pregunta al usuario si desea seguir jugando
-        keepPlaying = True
-        while keepPlaying:
-            print("¿Quieres seguir jugando?")
-            print("a. Sí")
-            print("b. No")
-            answer = input().lower().replace(" ", "")
-            if answer == "a":
-                keepPlaying, winner, play = False, True, True
-            elif answer == "b":
-                print("Vuelve pronto...")
-                keepPlaying, winner, play = False, True, False
+                if round >= 3 and checkWin(board):
+                    whoWin = player2
+                    pointsO += 1
+                    break               
+                if round >= 3: 
+                    if not isThereAWinner(board): #Se comprueba si aun hay posibilidades de ganar despues de cada vuelta.
+                        print("¡Es un empate!")
+                        break 
+                round+=1
+            if checkWin(board):
+                print(f"{whoWin} gano")
+            if whoWin == player1:
+                roundWinX.append(whoWin)
+                roundWinO = []
+                whoWin = ""
+            elif whoWin == player2:
+                roundWinO.append(whoWin)
+                roundWinX = []
+                whoWin = ""
             else:
-                print("Esta opción no está disponible. Elige otra opción")  
+                roundWinO = []
+                roundWinX = []
+                whoWin = ""
+            
+            if roundWinX and len(roundWinX) == 2:
+                roundWinX = []
+                whoWin = ""
+                pointsX *= 3
+
+            elif roundWinO and len(roundWinO) == 2:
+                roundWinO = []
+                pointsO *= 3
+                whoWin = ""
+
+            print("")
+            print(f"{player1} -> {pointsX}")
+            print(f"{player2} -> {pointsO}")
+
+            # Pregunta al usuario si desea seguir jugando
+            keepPlaying = True
+            while keepPlaying:
+                print("¿Quieres seguir jugando?")
+                print("a. Sí")
+                print("b. No")
+                answer = input().lower().replace(" ", "")
+                if answer == "a":
+                    keepPlaying, winner, play = False, True, True
+                elif answer == "b":
+                    print("Vuelve pronto...")
+                    keepPlaying, winner, play = False, True, False
+                else:
+                    print("Esta opción no está disponible. Elige otra opción")  
